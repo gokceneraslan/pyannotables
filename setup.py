befdd0a5b0122c6aa4e87ae05b287d53caeb9fdc
Gökçen Eraslan <gokcen.eraslan@gmail.com>
2019-12-22 15:51:47 -0500

    Fix HGNC merge

--- setup.py
+++ setup.py
@@ -38,6 +38,6 @@ setup(
     package_data={'': ['datafile_*']},
     setup_requires=setup_requirements,
     url='https://github.com/gokceneraslan/pyannotables',
-    version='0.1.0',
+    version='0.1.1',
     zip_safe=False,
 )
